68dfffc0d52ed8bf194e5aeed0cbc070f4e05019
Dri0m <dri0m@seznam.cz>
2021-06-12 16:41:01 +0200

    implement batch operations in the UI

--- static/js.js
+++ static/js.js
@@ -1,20 +1,67 @@
 function sendDelete(url) {
     if (confirm(`Are you sure you want to (soft) delete '${url}'?`)) {
-        let request = new XMLHttpRequest()
-        request.open("DELETE", url, false)
+        sendRequest(url, "DELETE")
+    }
+}
 
-        try {
-            request.onload = function () {
-                if (request.status !== 204) {
-                    alert(`failed to delete '${url}' - status ${request.status}`)
-                } else {
-                    alert(`delete of '${url}' successful`)
-                    location.reload()
-                }
-            };
-            request.send()
-        } catch (err) {
-            alert(`failed to delete '${url}' - exception '${err.message}'`)
+function sendRequest(url, method) {
+    let request = new XMLHttpRequest()
+    request.open(method, url, false)
+
+    try {
+        request.onload = function () {
+            if (request.status !== 204) {
+                alert(`failed to ${method} '${url}' - status ${request.status}`)
+            } else {
+                alert(`${method} of '${url}' successful`)
+                location.reload()
+            }
+        };
+        request.send()
+    } catch (err) {
+        alert(`failed to ${method} '${url}' - exception '${err.message}'`)
+    }
+}
+
+function controlAllCheckboxes(cb, className) {
+    let checkboxes = document.getElementsByClassName(className);
+
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = cb.checked
+    }
+}
+
+function batchDownloadFiles(checkboxClassName, attribute) {
+    let checkboxes = document.getElementsByClassName(checkboxClassName);
+
+    let url = "/submission-file-batch/"
+
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            url += checkboxes[i].dataset[attribute] + ","
         }
     }
+
+    url = url.slice(0, -1)
+    window.location.href = url
+}
+
+function batchComment(checkboxClassName, attribute) {
+    let checkboxes = document.getElementsByClassName(checkboxClassName);
+
+    let url = "/submission-batch/"
+
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            url += checkboxes[i].dataset[attribute] + ","
+        }
+    }
+
+    url = url.slice(0, -1)
+    url += "/comment"
+
+    let form = document.getElementById("batch-comment")
+    form.action=url
+    form.method="POST"
+    form.submit()
 }
