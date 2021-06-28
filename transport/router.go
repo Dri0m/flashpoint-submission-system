a7eb06ef1eefa39a5a42da4db0e74a6fd31a7009
Dri0m <dri0m@seznam.cz>
2021-06-28 17:52:55 +0200

    add request type to router

--- transport/router.go
+++ transport/router.go
@@ -39,113 +39,157 @@ func (a *App) handleRequests(l *logrus.Logger, srv *http.Server, router *mux.Rou
 		return a.IsUserWithinResourceLimit(r, uid, constants.ResourceKeySubmissionID, 1)
 	}
 
+	// static file server
+	router.PathPrefix("/static/").Handler(
+		http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
 	// oauth
-	router.Handle("/auth", http.HandlerFunc(a.HandleDiscordAuth)).Methods("GET")
-	router.Handle("/auth/callback", http.HandlerFunc(a.HandleDiscordCallback)).Methods("GET")
+	router.Handle(
+		"/auth",
+		http.HandlerFunc(a.RequestWeb(a.HandleDiscordAuth))).
+		Methods("GET")
+	router.Handle(
+		"/auth/callback",
+		http.HandlerFunc(a.RequestWeb(a.HandleDiscordCallback))).
+		Methods("GET")
 
 	// logout
-	router.Handle("/logout", http.HandlerFunc(a.HandleLogout)).Methods("GET")
-
-	// file server
-	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	router.Handle(
+		"/logout",
+		http.HandlerFunc(a.RequestWeb(a.HandleLogout))).
+		Methods("GET")
 
 	// pages
-	router.Handle("/",
-		http.HandlerFunc(a.HandleRootPage)).Methods("GET")
+	router.Handle(
+		"/",
+		http.HandlerFunc(a.RequestWeb(a.HandleRootPage))).
+		Methods("GET")
 
-	router.Handle("/profile",
-		http.HandlerFunc(a.UserAuthMux(a.HandleProfilePage))).Methods("GET")
+	router.Handle(
+		"/profile",
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(a.HandleProfilePage)))).
+		Methods("GET")
 
-	router.Handle("/submit",
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleSubmitPage, muxAny(isStaff, isTrialCurator, isInAudit)))).Methods("GET")
+	router.Handle(
+		"/submit",
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleSubmitPage, muxAny(isStaff, isTrialCurator, isInAudit))))).
+		Methods("GET")
 
-	router.Handle("/submissions",
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleSubmissionsPage, muxAny(isStaff)))).Methods("GET")
+	router.Handle(
+		"/submissions",
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleSubmissionsPage, muxAny(isStaff))))).
+		Methods("GET")
 
-	router.Handle("/my-submissions",
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleMySubmissionsPage, muxAny(isStaff, isTrialCurator, isInAudit)))).Methods("GET")
+	router.Handle(
+		"/my-submissions",
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleMySubmissionsPage, muxAny(isStaff, isTrialCurator, isInAudit))))).
+		Methods("GET")
 
-	router.Handle(fmt.Sprintf("/submission/{%s}", constants.ResourceKeySubmissionID),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}", constants.ResourceKeySubmissionID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleViewSubmissionPage,
 			muxAny(isStaff,
 				muxAll(isTrialCurator, userOwnsSubmission),
-				muxAll(isInAudit, userOwnsSubmission))))).Methods("GET")
+				muxAll(isInAudit, userOwnsSubmission)))))).
+		Methods("GET")
 
-	router.Handle(fmt.Sprintf("/submission/{%s}/files", constants.ResourceKeySubmissionID),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}/files", constants.ResourceKeySubmissionID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleViewSubmissionFilesPage,
 			muxAny(isStaff,
 				muxAll(isTrialCurator, userOwnsSubmission),
-				muxAll(isInAudit, userOwnsSubmission))))).Methods("GET")
+				muxAll(isInAudit, userOwnsSubmission)))))).
+		Methods("GET")
 
 	// receivers
-	router.Handle("/submission-receiver",
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		"/submission-receiver",
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleSubmissionReceiver, muxAny(
 				isStaff,
 				isTrialCurator,
-				muxAll(isInAudit, userHasNoSubmissions))))).Methods("POST")
+				muxAll(isInAudit, userHasNoSubmissions)))))).
+		Methods("POST")
 
-	router.Handle(fmt.Sprintf("/submission-receiver/{%s}", constants.ResourceKeySubmissionID),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission-receiver/{%s}", constants.ResourceKeySubmissionID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleSubmissionReceiver,
 			muxAny(isStaff,
 				muxAll(isTrialCurator, userOwnsSubmission),
-				muxAll(isInAudit, userOwnsSubmission))))).Methods("POST")
+				muxAll(isInAudit, userOwnsSubmission)))))).
+		Methods("POST")
 
-	router.Handle(fmt.Sprintf("/submission-batch/{%s}/comment", constants.ResourceKeySubmissionIDs),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission-batch/{%s}/comment", constants.ResourceKeySubmissionIDs),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleCommentReceiverBatch, muxAny(
 				muxAll(isStaff, a.UserCanCommentAction),
 				muxAll(isTrialCurator, userOwnsAllSubmissions),
-				muxAll(isInAudit, userOwnsAllSubmissions))))).Methods("POST")
+				muxAll(isInAudit, userOwnsAllSubmissions)))))).
+		Methods("POST")
 
 	router.Handle("/api/notification-settings",
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleUpdateNotificationSettings, muxAny(isStaff, isTrialCurator, isInAudit)))).Methods("PUT")
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleUpdateNotificationSettings, muxAny(isStaff, isTrialCurator, isInAudit))))).
+		Methods("PUT")
 
-	router.Handle(fmt.Sprintf("/submission/{%s}/subscription-settings", constants.ResourceKeySubmissionID),
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleUpdateSubscriptionSettings, muxAny(isStaff, isTrialCurator, isInAudit)))).Methods("PUT")
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}/subscription-settings", constants.ResourceKeySubmissionID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleUpdateSubscriptionSettings, muxAny(isStaff, isTrialCurator, isInAudit))))).
+		Methods("PUT")
 
 	// providers
-	router.Handle(fmt.Sprintf("/submission/{%s}/file/{%s}", constants.ResourceKeySubmissionID, constants.ResourceKeyFileID),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}/file/{%s}", constants.ResourceKeySubmissionID, constants.ResourceKeyFileID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleDownloadSubmissionFile,
 			muxAny(isStaff,
 				muxAll(isTrialCurator, userOwnsSubmission),
-				muxAll(isInAudit, userOwnsSubmission))))).Methods("GET")
+				muxAll(isInAudit, userOwnsSubmission)))))).
+		Methods("GET")
 
-	router.Handle(fmt.Sprintf("/submission-file-batch/{%s}", constants.ResourceKeyFileIDs),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission-file-batch/{%s}", constants.ResourceKeyFileIDs),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleDownloadSubmissionBatch, muxAny(
 				isStaff,
-				muxAll(isTrialCurator, userOwnsAllSubmissions))))).Methods("GET")
+				muxAll(isTrialCurator, userOwnsAllSubmissions)))))).
+		Methods("GET")
 
-	router.Handle(fmt.Sprintf("/submission/{%s}/curation-image/{%s}.png", constants.ResourceKeySubmissionID, constants.ResourceKeyCurationImageID),
-		http.HandlerFunc(a.UserAuthMux(
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}/curation-image/{%s}.png", constants.ResourceKeySubmissionID, constants.ResourceKeyCurationImageID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
 			a.HandleDownloadCurationImage,
 			muxAny(isStaff,
 				muxAll(isTrialCurator, userOwnsSubmission),
-				muxAll(isInAudit, userOwnsSubmission))))).Methods("GET")
+				muxAll(isInAudit, userOwnsSubmission)))))).
+		Methods("GET")
 
 	// soft delete
-	router.Handle(fmt.Sprintf("/submission/{%s}/file/{%s}", constants.ResourceKeySubmissionID, constants.ResourceKeyFileID),
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleSoftDeleteSubmissionFile, muxAll(isDeleter)))).Methods("DELETE")
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}/file/{%s}", constants.ResourceKeySubmissionID, constants.ResourceKeyFileID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleSoftDeleteSubmissionFile, muxAll(isDeleter))))).
+		Methods("DELETE")
 
-	router.Handle(fmt.Sprintf("/submission/{%s}", constants.ResourceKeySubmissionID),
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleSoftDeleteSubmission, muxAll(isDeleter)))).Methods("DELETE")
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}", constants.ResourceKeySubmissionID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleSoftDeleteSubmission, muxAll(isDeleter))))).
+		Methods("DELETE")
 
-	router.Handle(fmt.Sprintf(
-		"/submission/{%s}/comment/{%s}", constants.ResourceKeySubmissionID, constants.ResourceKeyCommentID),
-		http.HandlerFunc(a.UserAuthMux(
-			a.HandleSoftDeleteComment, muxAll(isDeleter)))).Methods("DELETE")
+	router.Handle(
+		fmt.Sprintf("/submission/{%s}/comment/{%s}", constants.ResourceKeySubmissionID, constants.ResourceKeyCommentID),
+		http.HandlerFunc(a.RequestWeb(a.UserAuthMux(
+			a.HandleSoftDeleteComment, muxAll(isDeleter))))).
+		Methods("DELETE")
 
 	err := srv.ListenAndServe()
 	if err != nil {
