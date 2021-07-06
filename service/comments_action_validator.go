7788e9bd65762b56a1457103f928287000a6b40e
Dri0m <dri0m@seznam.cz>
2021-07-06 13:12:19 +0200

    add tests for comment action validator

---
+++ service/comments_action_validator.go
@@ -0,0 +1,169 @@
+package service
+
+import (
+	"fmt"
+	"github.com/Dri0m/flashpoint-submission-system/constants"
+	"github.com/Dri0m/flashpoint-submission-system/types"
+	"net/http"
+)
+
+func uidIn(uid int64, ids []int64) bool {
+	for _, assignedUserID := range ids {
+		if uid == assignedUserID {
+			return true
+		}
+	}
+	return false
+}
+
+func isActionValidForSubmission(uid int64, formAction string, submission *types.ExtendedSubmission) error {
+	markedAdded := false
+	sid := submission.SubmissionID
+
+	for _, distinctAction := range submission.DistinctActions {
+		if constants.ActionMarkAdded == distinctAction {
+			markedAdded = true
+			break
+		}
+	}
+
+	// don't let last uploader decide on the submission
+	if formAction == constants.ActionAssignTesting || formAction == constants.ActionUnassignVerification {
+		if uid == submission.LastUploaderID {
+			return perr(fmt.Sprintf("you are the uploader of the newest version of submission %d, so you cannot assign it", sid), http.StatusBadRequest)
+		}
+	}
+	if formAction == constants.ActionApprove {
+		if uid == submission.LastUploaderID {
+			return perr(fmt.Sprintf("you are the uploader of the newest version of submission %d, so you cannot approve it", sid), http.StatusBadRequest)
+		}
+	}
+	if formAction == constants.ActionRequestChanges {
+		if uid == submission.LastUploaderID {
+			return perr(fmt.Sprintf("you are the uploader of the newest version of submission %d, so you cannot request changes on it", sid), http.StatusBadRequest)
+		}
+	}
+	if formAction == constants.ActionVerify {
+		if uid == submission.LastUploaderID {
+			return perr(fmt.Sprintf("you are the uploader of the newest version of submission %d, so you cannot verify it", sid), http.StatusBadRequest)
+		}
+	}
+
+	// stop (or ignore) double actions
+	if formAction == constants.ActionAssignTesting {
+		if uidIn(uid, submission.AssignedTestingUserIDs) {
+			return perr(fmt.Sprintf("you are already assigned to test submission %d", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionUnassignTesting {
+		if !uidIn(uid, submission.AssignedTestingUserIDs) {
+			return perr(fmt.Sprintf("you are not assigned to test submission %d", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionAssignVerification {
+		if uidIn(uid, submission.AssignedVerificationUserIDs) {
+			return perr(fmt.Sprintf("you are already assigned to verify submission %d", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionUnassignVerification {
+		if !uidIn(uid, submission.AssignedVerificationUserIDs) {
+			return perr(fmt.Sprintf("you are not assigned to verify submission %d", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionApprove {
+		if uidIn(uid, submission.ApprovedUserIDs) {
+			return perr(fmt.Sprintf("you have already approved submission %d", sid), http.StatusBadRequest)
+		}
+
+	} else if formAction == constants.ActionRequestChanges {
+		if markedAdded {
+			return perr(fmt.Sprintf("submission %d is alrady marked as added so you cannot request changes on it, please submit a bug report or a pending fix if there is a problem with the submission", sid), http.StatusBadRequest)
+		}
+		if uidIn(uid, submission.RequestedChangesUserIDs) {
+			return perr(fmt.Sprintf("you have already requested changes on submission %d", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionVerify {
+		if uidIn(uid, submission.VerifiedUserIDs) {
+			return perr(fmt.Sprintf("you have already verified submission %d", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionMarkAdded {
+		if markedAdded {
+			return perr(fmt.Sprintf("submission %d is alrady marked as added so it cannot be marked again", sid), http.StatusBadRequest)
+		}
+	}
+
+	// don't let the same user assign the submission to himself for more than one type of assignment
+	if formAction == constants.ActionAssignTesting {
+		if uidIn(uid, submission.AssignedVerificationUserIDs) {
+			return perr(fmt.Sprintf("you are already assigned to verify submission %d so you cannot assign it for verification", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionAssignVerification {
+		if uidIn(uid, submission.AssignedTestingUserIDs) {
+			return perr(fmt.Sprintf("you are already assigned to test submission %d so you cannot assign it for testing", sid), http.StatusBadRequest)
+		}
+	}
+
+	// don't let the same user approve and verify the submission
+	if formAction == constants.ActionAssignTesting {
+		if uidIn(uid, submission.VerifiedUserIDs) {
+			return perr(fmt.Sprintf("you have already verified submission %d so you cannot assign it for testing", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionApprove {
+		if uidIn(uid, submission.VerifiedUserIDs) {
+			return perr(fmt.Sprintf("you have already verified submission %d so you cannot approve it", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionAssignVerification {
+		if uidIn(uid, submission.ApprovedUserIDs) {
+			return perr(fmt.Sprintf("you have already approved (tested) submission %d so you cannot assign it for verification", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionVerify {
+		if uidIn(uid, submission.ApprovedUserIDs) {
+			return perr(fmt.Sprintf("you have already approved (tested) submission %d so you cannot verify it", sid), http.StatusBadRequest)
+		}
+	}
+
+	// don't let users do actions without assigning first
+	if formAction == constants.ActionApprove {
+		if !uidIn(uid, submission.AssignedTestingUserIDs) {
+			return perr(fmt.Sprintf("you are not assigned to test submission %d so you cannot approve it", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionVerify {
+		if !uidIn(uid, submission.AssignedVerificationUserIDs) {
+			return perr(fmt.Sprintf("you are not assigned to verify submission %d so you cannot verify it", sid), http.StatusBadRequest)
+		}
+	}
+
+	// don't let users verify before approve
+	if formAction == constants.ActionAssignVerification {
+		if len(submission.ApprovedUserIDs) == 0 {
+			return perr(fmt.Sprintf("submission %d is not approved (tested) so you cannot assign it for verification", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionVerify {
+		if len(submission.ApprovedUserIDs) == 0 {
+			return perr(fmt.Sprintf("submission %d is not approved (tested) so you cannot verify it", sid), http.StatusBadRequest)
+		}
+	}
+
+	// don't let users assign submission they have already confirmed to be good
+	if formAction == constants.ActionAssignTesting {
+		if uidIn(uid, submission.ApprovedUserIDs) {
+			return perr(fmt.Sprintf("you have already approved submission %d so you cannot assign it for testing", sid), http.StatusBadRequest)
+		}
+	} else if formAction == constants.ActionAssignVerification {
+		if uidIn(uid, submission.VerifiedUserIDs) {
+			return perr(fmt.Sprintf("you have already verified submission %d so you cannot assign it for verification", sid), http.StatusBadRequest)
+		}
+	}
+
+	// don't let user mark submission as added until it's verified
+	if formAction == constants.ActionMarkAdded {
+		if len(submission.VerifiedUserIDs) == 0 {
+			return perr(fmt.Sprintf("submission %d is not verified so you cannot mark it as added", sid), http.StatusBadRequest)
+		}
+	}
+
+	// cannot assign if marked as added
+	if formAction == constants.ActionAssignTesting {
+		if markedAdded {
+			return perr(fmt.Sprintf("submission %d is alrady marked as added so you cannot assign it for testing, please submit a bug report or a pending fix if there is a problem with the submission", sid), http.StatusBadRequest)
+		}
+	}
+
+	return nil
+}
---
+++ service/comments_action_validator_test.go
@@ -0,0 +1,284 @@
+package service
+
+import (
+	"github.com/Dri0m/flashpoint-submission-system/constants"
+	"github.com/Dri0m/flashpoint-submission-system/types"
+	"testing"
+)
+
+func Test_isActionValidForSubmission(t *testing.T) {
+	type args struct {
+		uid        int64
+		formAction string
+		submission *types.ExtendedSubmission
+	}
+
+	var commenterID int64 = 1
+	var lastUploaderID int64 = 2
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "uploader cannot assign for testing",
+			args: args{
+				uid:        lastUploaderID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{LastUploaderID: lastUploaderID},
+			},
+			wantErr: true,
+		},
+		{
+			name: "uploader cannot assign for verification",
+			args: args{
+				uid:        lastUploaderID,
+				formAction: constants.ActionAssignVerification,
+				submission: &types.ExtendedSubmission{LastUploaderID: lastUploaderID},
+			},
+			wantErr: true,
+		},
+		{
+			name: "uploader cannot approve",
+			args: args{
+				uid:        lastUploaderID,
+				formAction: constants.ActionApprove,
+				submission: &types.ExtendedSubmission{LastUploaderID: lastUploaderID},
+			},
+			wantErr: true,
+		},
+		{
+			name: "uploader cannot request changes",
+			args: args{
+				uid:        lastUploaderID,
+				formAction: constants.ActionRequestChanges,
+				submission: &types.ExtendedSubmission{LastUploaderID: lastUploaderID},
+			},
+			wantErr: true,
+		},
+		{
+			name: "uploader cannot verify",
+			args: args{
+				uid:        lastUploaderID,
+				formAction: constants.ActionVerify,
+				submission: &types.ExtendedSubmission{LastUploaderID: lastUploaderID},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot double assign for testing",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{AssignedTestingUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot double unassign for testing",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionUnassignTesting,
+				submission: &types.ExtendedSubmission{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot double assign for verification",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignVerification,
+				submission: &types.ExtendedSubmission{AssignedVerificationUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot double unassign for verification",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionUnassignVerification,
+				submission: &types.ExtendedSubmission{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot double approve",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionApprove,
+				submission: &types.ExtendedSubmission{ApprovedUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot double request changes",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionRequestChanges,
+				submission: &types.ExtendedSubmission{RequestedChangesUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for testing that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot unassign for testing that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionUnassignTesting,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for verification that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignVerification,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot unassign for verification that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionUnassignVerification,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot request changes that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionRequestChanges,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot approve that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionApprove,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot verify that's marked as added",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionVerify,
+				submission: &types.ExtendedSubmission{DistinctActions: []string{constants.ActionMarkAdded}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for both at once 1",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{AssignedVerificationUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for both at once 2",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignVerification,
+				submission: &types.ExtendedSubmission{AssignedTestingUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for verification if he tested",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignVerification,
+				submission: &types.ExtendedSubmission{AssignedTestingUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot approve if he is not assigned for testing",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionApprove,
+				submission: &types.ExtendedSubmission{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot verify if he is not assigned for verification",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionApprove,
+				submission: &types.ExtendedSubmission{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot mark as added if it is not verified",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionMarkAdded,
+				submission: &types.ExtendedSubmission{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for testing if they approved",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{ApprovedUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for verification if they verified",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{VerifiedUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for testing if they verified",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignTesting,
+				submission: &types.ExtendedSubmission{VerifiedUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+		{
+			name: "user cannot assign for verification if they approved",
+			args: args{
+				uid:        commenterID,
+				formAction: constants.ActionAssignVerification,
+				submission: &types.ExtendedSubmission{ApprovedUserIDs: []int64{commenterID}},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := isActionValidForSubmission(tt.args.uid, tt.args.formAction, tt.args.submission); (err != nil) != tt.wantErr {
+				t.Errorf("isActionValidForSubmission() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
